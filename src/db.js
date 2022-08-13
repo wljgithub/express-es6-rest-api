fd44f4e1d7a53f7ee64adda79a82b7888f50f088
v_ljjlwu <v_ljjlwu@tencent.com>
2022-08-13 16:24:44 +0800

    refactor: 初始化db实例

--- src/db.js
+++ src/db.js
@@ -1,4 +1,21 @@
-export default callback => {
-	// connect to a database if needed, then pass it to `callback`:
-	callback();
-}
+import config from "./config.json";
+import { Sequelize } from "sequelize";
+
+export default (callback) => {
+  // 从配置文件读取db配置
+  let dbConfig = config.db;
+
+  // 初始化并连接数据库
+  const sequelize = new Sequelize(
+    dbConfig.dbname,
+    dbConfig.user,
+    dbConfig.password,
+    {
+      host: dbConfig.host,
+      dialect: dbConfig.dialect,
+    }
+  );
+
+  // 返回数据实例
+  callback(sequelize);
+};
